90af48770c32562e94dc1a69b85529f653d4c224
daluxer-demiurge <0604411@gmail.com>
2021-04-17 20:32:09 +0400

    Ошибка по библиотеке в Bin/Debag Не понимаю что не так... уже все перепробовал, даже АВ отключал...

---
+++ CS_Basics_Lesson7/Lesson7_WF_Udvoitel/WF_Udvoitel/WF_Udvoitel/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace WF_Udvoitel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnGame = new System.Windows.Forms.Button();
+            this.lblGameNumber = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(250, 84);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(84, 36);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(250, 117);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(84, 36);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "*2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(162, 130);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNumber.Location = new System.Drawing.Point(206, 26);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(60, 46);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "99";
+            this.lblNumber.Click += new System.EventHandler(this.lblNumber_Click);
+            // 
+            // btnGame
+            // 
+            this.btnGame.Location = new System.Drawing.Point(162, 210);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(75, 34);
+            this.btnGame.TabIndex = 4;
+            this.btnGame.Text = "Играть!";
+            this.btnGame.UseVisualStyleBackColor = true;
+            this.btnGame.Click += new System.EventHandler(this.btnGame_Click);
+            // 
+            // lblGameNumber
+            // 
+            this.lblGameNumber.AutoSize = true;
+            this.lblGameNumber.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGameNumber.Location = new System.Drawing.Point(137, 26);
+            this.lblGameNumber.Name = "lblGameNumber";
+            this.lblGameNumber.Size = new System.Drawing.Size(60, 46);
+            this.lblGameNumber.TabIndex = 6;
+            this.lblGameNumber.Text = "99";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(193, 178);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(13, 15);
+            this.lblCounter.TabIndex = 7;
+            this.lblCounter.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(403, 281);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblGameNumber);
+            this.Controls.Add(this.btnGame);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Удвоитель";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Label lblGameNumber;
+        public System.Windows.Forms.Label lblCounter;
+    }
+}
+
---
+++ CS_Basics_Lesson7/Lesson7_WF_Udvoitel/WF_Udvoitel/WF_Udvoitel/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Udvoitel
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnCommand1.Text = "+1";
+            btnCommand2.Text = "*2";
+            btnReset.Text = "Сброс";
+            lblNumber.Text = "0";
+            this.Text = "Удвоитель";
+            lblCounter.Text = "0";
+        }
+
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            lblCounter.Text = (int.Parse(lblCounter.Text) + 1).ToString();
+        }
+
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            lblCounter.Text = (int.Parse(lblCounter.Text) + 1).ToString();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "1";
+        }
+
+        private void btnGame_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int gameNumber = rnd.Next(50, 1000);
+            MessageBox.Show("Ваша задача получить число " + gameNumber + " с минимальным количеством операций.", "Внимание!", MessageBoxButtons.OK);
+            lblGameNumber.Text = gameNumber.ToString();
+        }
+
+        private void lblCounter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblNumber_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
