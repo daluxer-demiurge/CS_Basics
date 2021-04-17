6cb2f62173f06c11df95728e7cb72598f3ba6ee7
daluxer-demiurge <0604411@gmail.com>
2021-04-18 00:20:25 +0400

    Task2 start

---
+++ CS_Basics_Lesson7/Lesson7_GuessNumber/Lesson7_GuessNumber/Lesson7_GuessNumber/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson7_GuessNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
